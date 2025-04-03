2256c5b29b937679b9d194720ca8ac7d73c947aa
Ain-Jeong <ain.jeong@stu.ocsb.ca>
2025-04-03 01:49:30 +0000

    init

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Ain Jeong All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Ain Jeong
+// Created on: Apr 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function convert fahrenheit to celsius.
+ */
+function convertFahrenheitToCelsius() {
+  // input
+  const temperatureInFahrenheit = parseFloat(
+    document.getElementById("temperature-in-fahrenheit").value
+  )
+
+  // process
+  const temperatureInCelsius = (temperatureInFahrenheit - 32) * (5 / 9)
+
+  // output
+  document.getElementById("answer").innerHTML =
+    temperatureInFahrenheit + "°F = " + temperatureInCelsius.toFixed(3) + "°C"
+}
